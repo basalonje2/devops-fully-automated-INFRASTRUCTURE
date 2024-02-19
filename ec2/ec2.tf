df11b24daf37d03c24bad0ab62dfa3dbc7ddec6f
Lonje-Basame <basalonje2@gmail.com>
2024-02-19 10:10:14 -0500

    new pipeline

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -52,6 +52,12 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
